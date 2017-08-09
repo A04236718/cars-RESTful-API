bd35ac3585ec5b1eb9cdb4e75633d9ee9544c97d
Mike Hopper <drmikeh@gmail.com>
2017-08-09 11:21:44 -0400

    Added about page.

--- app.js
+++ app.js
@@ -20,6 +20,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(function(req, res, next) {
+  console.log('YIPPEEE');
+  next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
