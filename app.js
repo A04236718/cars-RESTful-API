89e112b9f5a582a799a02d6925fad90605133b4e
Mike Hopper <drmikeh@gmail.com>
2017-08-10 09:20:40 -0400

    Completed RESTful routes with server-side rendering.

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var cars = require('./routes/cars');
 
 var app = express();
 
@@ -20,6 +22,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
+
+app.use(function(req, res, next) {
+  if (req.query._method && req.query._method.toUpperCase() === 'DELETE') {
+    req.method = 'DELETE';
+    req.url = req.path;
+  }
+  next();
+});
 
 app.use(function(req, res, next) {
   console.log('YIPPEEE');
@@ -30,6 +41,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/cars', cars);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
